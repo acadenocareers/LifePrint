c52a0a3bdca89beb5fd3ece6e0787f4e190bec1e
acadenocareers <acadenocareers@gmail.com>
2026-01-06 15:08:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,5 +123,5 @@ def predict_options():
 # App Runner
 # --------------------------------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
